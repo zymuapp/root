217061e95838b96c29444914cc69137b676320d2
jerembdn <work@jeremybdn.fr>
2025-03-28 11:53:01 +0100

    ✨ chore: add editorconfig and vscode settings for consistent formatting and recommended extensions

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,21 +1,21 @@
-"use client";
+'use client'
 
-import type { ReactNode } from "react";
+import type { ReactNode } from 'react'
 
 interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+  children: ReactNode
+  className?: string
+  appName: string
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
-      type="button"
+      type='button'
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
     </button>
-  );
-};
+  )
+}
